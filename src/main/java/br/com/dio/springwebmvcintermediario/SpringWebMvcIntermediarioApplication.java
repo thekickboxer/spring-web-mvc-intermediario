2c8351e101edeaef3a7084bcec929f0736edc90f
thekickboxer <leo_kickboxer@hotmail.com>
2021-08-16 10:29:40 -0300

    Projeto inicial

---
+++ src/main/java/br/com/dio/springwebmvcintermediario/SpringWebMvcIntermediarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.springwebmvcintermediario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcIntermediarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcIntermediarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/springwebmvcintermediario/SpringWebMvcIntermediarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.springwebmvcintermediario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvcIntermediarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
